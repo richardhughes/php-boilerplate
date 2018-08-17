b98c06c52892726c7a7d10fdef6448ef472742bf
Richard Hughes <eeuc40@hotmail.com>
2018-08-17 21:45:20 +0100

    Add code to convert to a starter project

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSuccessfullyInstalled()
+    {
+        $this->assertTrue(true);
+    }
+}
